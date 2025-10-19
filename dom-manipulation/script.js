783edbf48e527f5bdbf9dead2b2822654a3b5086
AureliaBett <cbettaurelia@gmail.com>
2025-10-19 09:09:07 +0300

    Generating random qoutes

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,4 +1,4 @@
-const quote = [
+const quotes = [
     {text: "this one", category: 1},
     {text: "this two", category: 2},
     {text: "this three", category: 3},
@@ -6,8 +6,35 @@ const quote = [
     {text: "this five", category: 5},
     
 ]
-const quoteButton = document.getElementById("newQuote");
-const handleClick = function(){
-    alert("quote[0]")
+
+const newQuoteText = document.getElementById("newQuoteText");
+const newQuoteCategory = document.getElementById("newQuoteCategory");
+
+
+function showRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+  const display = document.getElementById("quoteDisplay");
+  display.textContent = `"${randomQuote.text}" — (${randomQuote.category})`;
 }
-quoteButton.addEventListener("click", handleClick)
+document.getElementById("newQuote").addEventListener("click", showRandomQuote);
+
+function addQuote() {
+  const textInput = document.getElementById("newQuoteText").value.trim();;
+  const categoryInput = document.getElementById("newQuoteCategory").value.trim();
+
+
+  if (textInput === "" || categoryInput === "") {
+    alert("Please enter both a quote and a category.");
+    return;
+  }
+  quotes.push({ textInput, categoryInput });
+  
+  textInput.value = "";
+  categoryInput.value = "";
+
+  const display = document.getElementById("quoteDisplay");
+  display.textContent = `New quote added: "${textInput}" — (${categoryInput})`;
+}
+
+document.getElementById("addQuoteBtn").addEventListener("click", addQuote);
